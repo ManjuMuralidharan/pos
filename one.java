dd2d703fbf8eea3af5af7942798911e069c1ddf5
ManjuMuralidharan <manju1695@gmail.com>
2016-08-09 18:18:00 +0530

    Add files via upload

---
+++ one.java
@@ -0,0 +1,24 @@
+import java.util.*;
+
+public class one {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Scanner s=new Scanner(System.in);
+		int n=s.nextInt();
+
+		if( n == 0)
+	    { 
+			System.out.println("Number is equal to zero");
+			}
+	    else if (n > 0)
+	    { 
+	    	System.out.println("Number is positive"); 
+	    	}
+	    else 
+	    { 
+	    	System.out.println("Number is negative"); 
+	    	}
+			}
+
+}
